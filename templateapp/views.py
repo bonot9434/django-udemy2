e212435ad73c7c63211a2404c723bc4f1756eefb
bonot9434 <bonot9434@gmail.com>
2021-12-03 23:17:11 +0900

    92.home作成

--- templateapp/views.py
+++ templateapp/views.py
@@ -1,5 +1,20 @@
+from django.http import request
 from django.shortcuts import render
 
 # Create your views here.
 def index (request):
-  return render(request, 'index.html')
+  val = 'Goodbye'
+  return render(request, 'index.html', context={'value':val})
+
+def home (request):
+  my_name = 'Taro Yamada'
+  favorite_fruits = ['Apple','Grape','Lemon']
+  my_info = {
+    'name':'Taro',
+    'age': 18
+  }
+  return render(request, 'home.html', context = {
+    'my_name' : my_name,
+    'favorite_fruits' : favorite_fruits,
+    'my_info' : my_info
+  })
